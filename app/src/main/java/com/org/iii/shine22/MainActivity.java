17a25f87cbfc513eec26d22ae56854c8594e4800
user <a>
2016-11-29 12:22:51 +0800

    v2 三軸加速感應器

--- app/src/main/java/com/org/iii/shine22/MainActivity.java
+++ app/src/main/java/com/org/iii/shine22/MainActivity.java
@@ -7,6 +7,7 @@ import android.hardware.SensorManager;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
+import android.widget.TextView;
 
 import java.util.List;
 
@@ -14,11 +15,16 @@ public class MainActivity extends AppCompatActivity {
     private SensorManager smgr;
     private Sensor sensor;
     private MySensorListener listener;
+    private TextView v0, v1, v2;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        v0 = (TextView)findViewById(R.id.v0);
+        v1 = (TextView)findViewById(R.id.v1);
+        v2 = (TextView)findViewById(R.id.v2);
 
         smgr=(SensorManager)getSystemService(SENSOR_SERVICE);
 
@@ -29,13 +35,27 @@ public class MainActivity extends AppCompatActivity {
         sensor = smgr.getDefaultSensor(Sensor.TYPE_ACCELEROMETER);
         listener = new MySensorListener();
     }
+
+
     private class MySensorListener implements SensorEventListener{
         @Override
         public void onSensorChanged(SensorEvent sensorEvent) {
-
+            float[] values = sensorEvent.values;
+            v0.setText("X: "+values[0]);
         }
 
         @Override
         public void onAccuracyChanged(Sensor sensor, int i) {}
+    }
+    @Override
+    protected void onStart() {
+        super.onStart();
+        smgr.registerListener(listener,sensor,SensorManager.SENSOR_DELAY_NORMAL);
+    }
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+        smgr.unregisterListener(listener);
     }
 }
